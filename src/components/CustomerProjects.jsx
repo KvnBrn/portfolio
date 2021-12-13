c32f591a2901de1bde6e309566420bb9d99e0b5f
Kevin BRUNO <kevinbruno90@gmail.com>
2021-12-13 14:13:20 +0100

    add personal project

--- src/components/CustomerProjects.jsx
+++ src/components/CustomerProjects.jsx
@@ -8,7 +8,7 @@ const CustomerProjects = () => {
       <h2>Customer Projects</h2>
       <div className="all-customer-projects">
         <Project 
-          projetImg={''}
+          projetImg={'https://lh3.googleusercontent.com/proxy/LQnTT30yFkeRgmkdyMA1TO8GoTDQz6N1X9vSVVqyPBKBBtR5z8ZF6TKxTX1Zb2FVAEMrkwnyK5roE_ozBHOYCxcUwKXEijUrpgPgbvDe-QCrDU0b2SNZo8lVhQ2zD8lNDR-bOCH4arF6U7I'}
           projetLink={webLinkIcon}
           projetUrl={''}
           projetTitle={'The Good Loop'}
--- src/components/PersonalProjects.jsx
+++ src/components/PersonalProjects.jsx
@@ -1,3 +1,6 @@
+import Project from './Project';
+import webLinkIcon from '../assets/webLinkIcon.jpg';
+import portfolio from '../assets/portfolio.png';
 import './PersonalProjects.css';
 
 const PersonalProjects = () => {
@@ -5,6 +8,14 @@ const PersonalProjects = () => {
     <div className="PersonalProjects">
       <h2>Personal Projects</h2>
       <div className="all-personal-projects">
+        <Project 
+            projetImg={portfolio}
+            projetLink={webLinkIcon}
+            projetUrl={'https://pensive-dijkstra-4911a7.netlify.app'}
+            projetTitle={'Portfolio'}
+            projetDescription={'Of course !'}
+            projetLanguages={'Languages used: React, CSS, JS.'}
+          />
       </div>
     </div>
   );
